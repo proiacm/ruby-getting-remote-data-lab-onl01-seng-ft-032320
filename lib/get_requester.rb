8949c77fe30244f15d3f3fd4b845cf28f3260d41
CiaraMaria Proia <proiacm@gmail.com>
2020-04-09 21:23:54 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,12 +5,14 @@ require 'json'
 class GetRequester
   
 
-  def initialize
-
+  def initialize(url)
+    @url = url
   end
     
   def get_response_body
-      
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
   end
   
   def parse_json
