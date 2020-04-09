4ab925b6242eaf52d5d76d56f40e2bc4ed02311d
CiaraMaria Proia <proiacm@gmail.com>
2020-04-09 21:23:10 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,18 @@
-# Write your code here
+require 'open-uri'
+require 'net/http'
+
+class GetRequester
+  
+
+  def initialize
+
+  end
+    
+  def get_response_body
+ 
+  end
+  
+  def parse_json
+  end 
+  
+end
